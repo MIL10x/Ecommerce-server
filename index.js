bc90007c6eacf9eb3a6701116304d43466a3ba50
MILTON VINCILINE V <miltonvinciline08@gmail.com>
2024-10-31 04:05:32 +0530

    test1

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import express from "express";
 import jsonServer from 'json-server';
-import cors from 'cors'; // Optional, but recommended
 
 const server = express();
-server.use(cors()); // Use the cors middleware
-
-// Middleware to set CORS headers
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
-// Set up the router
 const router = jsonServer.router('./data/db.json');
 server.use('/api', router);
-server.db = router.db; // Optional, if you need to access the db directly
+server.db = router.db; 
 
 server.use(router)
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+
+server.listen(3000);
